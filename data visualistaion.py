875866b09247d0c5c4d669392b11dc030dd1a50e
PablArb <pab.arbona@gmail.com>
2022-08-17 19:54:36 +0200

    modif mineurs

--- data visualistaion.py
+++ data visualistaion.py
@@ -7,7 +7,7 @@ def str2float(N:str):
     else :
         return float(N)
 
-dos = open('Desktop/Test/video test/csv/positions objets.csv', 'r')
+dos = open('Desktop/data/test 3/csv/positions objets.csv', 'r')
 
 lines = dos.readlines()
 T = []
@@ -15,8 +15,8 @@ X0 = []
 Y0 = []
 X1 = []
 Y1 = []
-X2 = []
-Y2 = []
+# X2 = []
+# Y2 = []
 
 for line in lines[1:] :
 
@@ -24,14 +24,14 @@ for line in lines[1:] :
     time = str2float(line[1])
     x0, y0 = str2float(line[2]), str2float(line[3])
     x1, y1 = str2float(line[4]), str2float(line[5])
-    x2, y2 = str2float(line[6]), str2float(line[7])
+    # x2, y2 = str2float(line[6]), str2float(line[7])
     T.append(time)
     X0.append(x0)
     Y0.append(y0)
     X1.append(x1)
     Y1.append(y1)
-    X2.append(x2)
-    Y2.append(y2)
+    # X2.append(x2)
+    # Y2.append(y2)
 
 Xrel = [ X0[i]-X1[i] for i in range( len(X0) ) ]
 Yrel = [ Y0[i]-Y1[i] for i in range( len(Y0) ) ]
@@ -41,9 +41,9 @@ plt.figure('position')
 plt.clf()
 ax = plt.axes(projection='3d')
 
-ax.plot(T, X0, Y0, label='obj0')
-ax.plot(T, X1, Y1, label='obj1')
-ax.plot(T, X2, Y2, label='obj2')
+ax.plot(T, Xrel, Yrel, label='obj0')
+# ax.plot(T, X1, Y1, label='obj1')
+# ax.plot(T, X2, Y2, label='obj2')
 
 plt.legend()
 plt.show()
