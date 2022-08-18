d540bc08fa8a0d64c8f667896379301faa1cbbc6
PablArb <pab.arbona@gmail.com>
2022-08-18 18:15:42 +0200

    post création du readme

--- projet_py-analyse_image.py
+++ projet_py-analyse_image.py
@@ -1,15 +1,3 @@
-# Cet algorythme permet de localiser les repères de couleur rouge, vert ou bleu présents sur la vidéo mise en entrée,
-# fonctionne avec le format mp4.
-
-# Fonctionne sur mac avec python v3.9 avec les modules pymediainfo, numpy et cv2 installés.
-
-# Listes des variables :
-# c                 : (entier) couleur des repères étudiés.
-# positions         : (dictionaire dont chaque clé correspond à une frame et la valeure associée est un dictionaire
-# comprends les positions des objets détectés) positions des repères détectés à chacune des frames de la vidéo.
-# tracked_objects : idem que positions seulement désormais les objets sont suivis et non plus identifiés par leur
-# ordre de découverte.
-
 # Import modules
 import pymediainfo as mi
 import numpy as np
