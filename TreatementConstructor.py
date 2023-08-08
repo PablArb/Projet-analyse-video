6de8b9b792acd21b417a2a94bec90d07358f1de3
PablArb <pab.arbona@gmail.com>
2023-08-08 20:44:40 +0200

    test

---
+++ TreatementConstructor.py
@@ -0,0 +1,9 @@
+from SettingsConstructor import Settings
+# from ExecutionStatus import Status
+
+
+
+class TreatementSpecs(object):
+    def __init__(self):
+        self.settings = Settings()
+        # self.status = Status()
